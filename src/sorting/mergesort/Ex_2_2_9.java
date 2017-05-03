5dc339a6fa9ea75ed0808299a278381dfd7b16a2
Patrik Nygren <patriknygren82@gmail.com>
2017-05-03 18:16:52 +0200

    Ex_2_2_9

---
+++ src/sorting/mergesort/Ex_2_2_9.java
@@ -0,0 +1,51 @@
+package sorting.mergesort;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+/**
+ * Created by patriknygren on 2017-05-03.
+ */
+public class Ex_2_2_9 {
+
+    public static void main(String[] args) {
+        Integer[] array = {0, 9, 2, 7, 6, 4, 3, 8, 1, 5, 11, 12, 14, 18, 16, 17, 19, 10, 13, 15};
+        Merge.sort(array);
+        Arrays.stream(array).forEach(x -> StdOut.print(x + " "));
+    }
+
+    public static class Merge {
+        // private static Comparable[] aux;
+
+        public static void sort(Comparable[] a) {
+            Comparable[] aux = new Comparable[a.length];
+            sort(a, 0, a.length - 1, aux);
+        }
+
+        private static void sort(Comparable[] a, int lo, int hi, Comparable[] aux) {
+            if (hi <= lo) return;
+            int mid = lo + (hi - lo)/2;
+            sort(a, lo, mid, aux);
+            sort(a, mid+1, hi, aux);
+            merge(a, lo, mid, hi, aux);
+        }
+
+        private static void merge(Comparable[] a, int lo, int mid, int hi, Comparable[] aux) {
+            for (int k = lo; k <= hi; k++)
+                aux[k] = a[k];
+            int i = lo;
+            int j = mid+1;
+            for (int k = lo; k <= hi; k++) {
+                if (i > mid)                      a[k] = aux[j++];
+                else if (j > hi)                a[k] = aux[i++];
+                else if (less(aux[i], aux[j]))  a[k] = aux[i++];
+                else                            a[k] = aux[j++];
+            }
+        }
+
+        private static boolean less(Comparable x, Comparable y) {
+            return x.compareTo(y) < 0;
+        }
+    }
+}
